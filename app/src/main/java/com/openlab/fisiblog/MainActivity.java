adbe0be9133b371c4dd3b721b630799b1496f468
Junior <juniorquegu@gmail.com>
2017-05-06 15:55:34 -0500

    First commit

---
+++ app/src/main/java/com/openlab/fisiblog/MainActivity.java
@@ -0,0 +1,26 @@
+package com.openlab.fisiblog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static  final String URL_1= "http://img13.deviantart.net/60ba/i/2017/014/a/4/leaves_on_skin_by_agnes_cecile-davcpfw.jpg";
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.id_image);
+
+        Glide.with(this).load(URL_1).transform(new CircleTransform(this)).into(imageView);
+
+
+
+
+    }
+}
