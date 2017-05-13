698d9440bf661b4412d15076171d107aa189f2e4
Junior <juniorquegu@gmail.com>
2017-05-13 15:33:05 -0500

    Button Material

--- app/src/main/java/com/openlab/fisiblog/MainActivity.java
+++ app/src/main/java/com/openlab/fisiblog/MainActivity.java
@@ -13,11 +13,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_secondary);
+/*
 
         imageView = (ImageView) findViewById(R.id.id_image);
 
         Glide.with(this).load(URL_1).transform(new CircleTransform(this)).into(imageView);
+*/
 
 
 
